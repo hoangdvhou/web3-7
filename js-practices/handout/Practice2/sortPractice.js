80c078a02630687d5045d1324142bd92096018b6
hoangdvhou <hoangdv.hou@gmail.com>
2018-11-03 11:06:27 +0700

    Buoi 1

---
+++ js-practices/handout/Practice2/sortPractice.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function sort(input) {
+  //return input.sort((a,b) => a-b); // Remove this line and change to your own algorithm
+    for (var i = 0; i < input.length - 1; i++) {
+        for (var j = i + 1; j < input.length; j++) {
+            if (input[i] > input[j]) {
+                var temp;
+                temp = input[i];
+                input[i] = input[j];
+                input[j] = temp;
+            }
+        }
+    }
+    return input
+}
+
+module.exports = sort
