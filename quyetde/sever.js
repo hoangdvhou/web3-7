8eafa31adfc831f30ab249dc994c551052029415
hoangdvhou <hoangdv.hou@gmail.com>
2018-11-29 22:42:43 +0700

    complete quyetde

--- quyetde/sever.js
+++ quyetde/sever.js
@@ -1,46 +1,128 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser')
+const express = require("express");
+var bodyParser = require('body-parser');
 const fs = require("fs");
+const mongoese = require("mongoose");
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended : false}));
-app.get("/ask", (req,res) =>{
-    res.sendFile(__dirname + "/view/ask.html");
+const questionModel = require("./models/questionModel");
+
+questionModel.find({}, (err, question) => {
+    if(err) console.log(err)
+    else console.log("List question: ", question);
 });
 
-app.post("/ask",(req,res) =>{
-        console.log(req.body);
-        const questions = JSON.parse(fs.readFileSync('./question.json','utf-8'));
-        console.log(questions,questions.length);
-        let newQuestion = {
-            id: questions.length,
-            yes: 0,
-            no: 0,
-            content: req.body.question
-        }
-        questions.push(newQuestion);
-        fs.writeFileSync('./question.json',JSON.stringify(questions));
-        res.redirect("/");
-    });
+mongoese.connect("mongodb://localhost/firstapp",
+    { useNewUrlParser: true },
+    (err) => {
+        if(err) console.log(err)
+        else console.log("Success!");
+});
 
-    app.get('/randomquestion', (req, res) => {
-        const rnd = Math.floor(Math.random() * questions.length);
-        res.send(questions[rnd].content);
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.get('/ask', (req,res)=>{
+    res.sendFile(__dirname + "/views/ask.html");
+});
+
+app.post('/ask', (req,res)=>{
+    console.log(req.body);
+    // console.log("post data");
+    // req.on("data", (data) => {
+    //     console.log(String(data).split("=")[1]);
+    // })
+    // const questions = JSON.parse(fs.readFileSync('./questions.json', 'utf-8'));
+    // console.log(questions, questions.length);
+    // let newQuestion = {
+    //     id: questions.length,
+    //     yes: 0,
+    //     no: 0,
+    //     content: req.body.question
+    // }
+    // questions.push(newQuestion);
+    // fs.writeFileSync("./questions.json", JSON.stringify(questions));
+    // res.redirect('/');
+
+    questionModel.create({content: req.body.question}, (err, questionCreated) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(questionCreated);
+            res.redirect("/");
+        }
     })
+});
+
+app.get("/", (req,res)=> {
+    res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get('/randomquestion', (req,res) => {
+    questionModel.countDocuments({}, (err, count) => {
+        if(err) console.log(err)
+        else {
+            const randomNum = Math.floor(Math.random()*count);
+            questionModel.findOne({}, null, { skip: randomNum }, (err, questionFound) => {
+                if(err) console.log(err)
+                else res.send(questionFound);
+            })
+        }
+    })
+    // const arrQues = JSON.parse(fs.readFileSync('./questions.json', 'utf-8'));
+    // console.log(arrQues);
+    // res.send(arrQues[Math.floor(Math.random() * arrQues.length)]);
+});
+
+app.post('/answer', (req,res)=>{
+    const vote = req.body.vote;
+    const questionId = req.body.questionId;
+    // const questions = JSON.parse(fs.readFileSync('./questions.json', 'utf-8')); 
     
-    app.get('/question/:questionid', (req, res) => {
-        const {
-            questionid
-        } = req.params;
-        res.send(questions[questionid].content);
-    });
-app.get("/",(req,res) => {
-    res.send("Hello");
+    questionModel.findOne({_id : questionId}, function (err, doc){
+        if (vote=="yes") {
+            doc.yes +=1;
+        } else {
+            doc.no +=1;
+        }
+        doc.save((err) => {
+            // res.redirect("http://localhost:3000/question/"+questionId);
+            res.send(doc);
+        });
+      });
+    
+    // if (vote=="yes") {
+    //     questions[questionId].yes +=1;
+    // } else {
+    //     questions[questionId].no +=1;
+    // }
+    // fs.writeFileSync('./questions.json', JSON.stringify(questions));
+    // res.redirect("http://localhost:3000/question/"+questionId);
+
+    
+    
 })
 
-app.use(express.static("view"));
-app.listen(1605,(err) => {
-    if(err) console.log(err)
-    else console.log("Success");
-}
-);
+app.get('/:questionId', (req,res) => {
+    const questionId = req.params.questionId;
+    res.sendFile(__dirname + "/views/questionResult.html");
+});
+
+app.get('/question/:questionId', (req,res) => {
+    const questionId  = req.params.questionId;
+    // const arrQues = JSON.parse(fs.readFileSync('./questions.json', 'utf-8'));
+    // const data = arrQues[questionId];
+    // res.send(data);
+    // res.sendFile(__dirname + `/views/question.html`);
+    questionModel.findById(questionId, (err, questions)=> {
+        if(err) console.log(err)
+        else res.send(questions);
+    })
+});
+
+app.use(express.static("views"));
+app.listen(3000, (err)=>{
+    if (err) {
+        console.log("error!!!")
+    } else {
+        console.log("start success!");
+    }
+});
--- quyetde/view/ask.js
+++
@@ -1,6 +0,0 @@
-const totalChar = 200;
-
-document.getElementById("textInput").addEventListener("input",function(){
-    let remainChar = totalChar - document.getElementById("textInput").value.length;
-    document.getElementById("remain").innerText = "còn " + remainChar + "/200 ký tự";
-})
